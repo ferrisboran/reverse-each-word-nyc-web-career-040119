6b2209e37cf4bf893078dcd7a05702b3860c7320
Ferris Boran <ferris.boran@gmail.com>
2019-03-11 21:22:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   array.collect do |word|
     word.reverse
-  end
-  return array.join(" ")
+  end.join(" ")
 =begin
 	yarra = []
 	array.each do |word|
